12fe16cda7a6a0459df212add221af72063dae56
Wilson Cruz <galatoide@gmail.com>
2020-03-14 15:24:42 +0000

    first commit

---
+++ js/ball.js
@@ -0,0 +1,8 @@
+function draw(){
+    ctx.beginPath();
+    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath;
+}
+setInterval(draw, 10);
---
+++ js/main.js
@@ -0,0 +1,6 @@
+let canvaas = document.getElementById('canvas');
+let ctx = canvaas.getContext('2d');
+
+
+
+console.log('testing')
